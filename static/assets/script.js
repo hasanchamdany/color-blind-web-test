118ecd221d8c3e58c94ec352cf6d4b3c1fd9c627
hasanchamdany <hasanchamdany@gmail.com>
2022-03-03 18:23:18 +0700

    fix back-end

--- static/assets/script.js
+++ static/assets/script.js
@@ -1,26 +1,34 @@
-let inputData = []
+let inputData = [];
 // console.log("test")
-const btn = document.getElementById("generate")
+const btn = document.getElementById("generate");
+const disease = document.getElementById("diseaseType");
 
 // for (var i = 0; i < 10; i++){
 //     inputData.push.getElementById(i).value
 // }
 
-btn.onclick = function(){
-    const form = document.getElementById("form")
-    // console.log(form["0"])
-    // console.log(form["0"].value)
-    for(var i = 0; i<10; i++){
-        inputData[i] = form[i].value
-    }
-    // console.log(inputData[0])
-    fetch('/test',{
-        method: 'POST',
-        body: JSON.stringify({
-            answers: inputData
-        })
-    }).then(result => result.json()).then(result => {
-        console.log(result)
-    })
-}
-
+btn.onclick = function () {
+  const form = document.getElementById("form");
+  // console.log(form["0"])
+  // console.log(form["0"].value)
+  for (var i = 0; i < 10; i++) {
+    inputData[i] = form[i].value;
+  }
+  // console.log(inputData[0])
+  fetch("/test", {
+    method: "POST",
+    body: JSON.stringify({
+      answers: inputData,
+    }),
+  })
+    .then((result) => result.json())
+    .then((result) => {
+      res = result.result;
+      console.log(res);
+      if (res.length === 0) {
+        disease.innerHTML = `<p>Your disease is not detected, please fill all of the answers</p>`;
+        return;
+      }
+      disease.innerHTML = `You are ${res}`;
+    });
+};
